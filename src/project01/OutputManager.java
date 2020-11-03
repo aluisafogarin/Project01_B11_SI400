f0b7bd45f00d369e61a45a951b79672f75f2b91b
Verussa De Alencar Caridi <vecaridi@gmail.com>
2020-11-02 21:27:55 -0300

    Add files via upload

--- src/project01/OutputManager.java
+++ src/project01/OutputManager.java
@@ -1,22 +1,40 @@
 package project01;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io. *;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.swing.JOptionPane;
 
 public class OutputManager {
-    private final String outPutFileName;
 
-    public OutputManager(String fileName) {
-        this.outPutFileName = fileName.substring(0, fileName.length() - 4) + ".csv";
+    private void orderMap() {
+        
     }
 
-    protected void recordOutputFile(ArrayList<String> data) throws IOException {
-    
-    }
-
-    public String outPutFileName() {
-        return this.outPutFileName;
-    }
+    private void recordOutputFile( String pathString_CSV, String Name_file_CSV) {
+        try {
+			PrintWriter pWriter = new PrintWriter(
+					new File(pathString_CSV)+"\\"+Name_file_CSV+".csv");
+			proper_word_outputs();// Output
+			pWriter.write(getbuilder_string().toString());
+			pWriter.close();
+            JOptionPane.showMessageDialog(null, "Finished writing the file: "+Name_file_CSV+" in the path "+pathString_CSV+"\\"+Name_file_CSV+".csv");
+            // Generated .csv file
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Error:" + e); //Error message
+		}
+	}
 }
+        /*
+        BufferedReader in = new BufferedReader (new FileReader ( " arquivo.txt "));
+        //cria um objeto virtual para a leitura de um arquivo de texto existente
+        conteúdo string;
+        while (( content = in.readLine ()) ! = null) { System.out.println
+            (conteúdo) ;
+        }
+        in.close ();
+    }
+}*/
